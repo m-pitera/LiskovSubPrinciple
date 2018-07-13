9452b5de6e16559453344816f65d5953472ae97c
m-pitera <mpitera483@gmail.com>
2018-07-12 23:57:59 -0500

    added other birds

--- bird.rb
+++ bird.rb
@@ -1,5 +1,18 @@
 class Bird
-  def set_location(longitue, latitude); end
-  def set_altitude(altitude); end
-  def draw(); end
+  def fly(); end
+  def something_else(something); end
+  def something_complicated(complicated); end
+  def something_a_bird_would_do(); end
+end
+
+class Toucan
+  # . . .
+end
+
+class HummingBird
+  # . . .
+end
+
+class Penguin
+  # . . .
 end
